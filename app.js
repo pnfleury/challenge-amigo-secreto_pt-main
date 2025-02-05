5e4c1810518c3ff50d9f5ab5fc2182ecb88a44b0
Paulo Fleury <pnfleury@gmail.com>
2025-02-05 19:40:45 -0300

    estilo de fonte para sorteado

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
 let listaAmigos = [];
+var x = document.querySelector ('h4');
 
 function adicionarAmigo() {
+    exibirTextoNaTela('p','Sortear Amigo')
     let amigos = document.querySelector ('input').value;
     if (amigos == '') {
-        alert ('DIGITE UM NOME VALIDO');
-    } else {
+        alert ('DIGITE UM NOME VALIDO');} 
+    else {
+            trocaClasse(x,'amigo-sorteado','lista-amigos');
             listaAmigos.push(amigos);
             amigos = document.querySelector ('input');
             amigos.value = '';
-            exibirTextoNaTela('h5',listaAmigos);
-            
-        }
+            exibirTextoNaTela('h4',listaAmigos);
+       }
     }
 
 function sortearAmigo() {
     sorteio = listaAmigos.length;
     if (sorteio == 0) {
-        alert ('ADICIONE UM NOME PRIMEIRO');
-        } else {
+        alert ('ADICIONE UM NOME PRIMEIRO');} 
+    else {
         let amigoSorteado = parseInt(Math.random() * sorteio);
-        exibirTextoNaTela('h3',listaAmigos[amigoSorteado]);
-        listaAmigos.splice(amigoSorteado, 1);
-        exibirTextoNaTela('h5',listaAmigos);
+        trocaClasse(x,'lista-amigos','amigo-sorteado');
+        exibirTextoNaTela('h4',listaAmigos[amigoSorteado]);
+        exibirTextoNaTela('p','Amigo Sorteado')
+        listaAmigos=[];        
     }
 }
 
@@ -32,4 +36,9 @@ function exibirTextoNaTela(tag, texto) {
     }
 
 
+function trocaClasse(elemento, antiga, nova) {
+    elemento.classList.remove(antiga);
+    elemento.classList.add(nova);
+}
 
+   
